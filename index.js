dbc023a2d94ec70f1206b25eb05e859c3b74fb2e
burcubese@gmail.com <burcubese@gmail.com>
2021-06-22 22:14:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+//Bildirimler yapılıyor
+var Number1,Number2,Operator,result,answer,True=0,False=0;
+
+//Html nesnelerine ulasma
+Number1=document.getElementById('Number1');
+Number2=document.getElementById('Number2');
+Operator=document.getElementById('Operator');
+result=document.getElementById('result');
+answer=document.getElementById('answer');
+True=document.getElementById('True');
+False=document.getElementById('False');
+
+
+//Rastgele sayı üretme fonksiyonu
+function RandomNumber (min,max) {
+    var number=Math.floor(Math.random()*(max-min))+min;
+    return number;
+}
+
+//Oyun baslangicinda veya soru tahmini sonrasi yeni soru olusturma fonksiyonu
+function newQuestion(){
+    var operation=["+","-","*","/"];
+    Operator.textContent=operation[RandomNumber(0,4)];
+    Number1.textContent=RandomNumber(0,50);
+    Number2.textContent=RandomNumber(0,50);
+
+    //Kalansiz bölme islemi yapacak kosul
+    if(Operator.textContent=="/"){
+        while(true){
+            Number2.textContent=RandomNumber(0,50);
+            if(Number1.textContent % Number2.textContent==0){
+                break;
+            }
+        }
+    }
+}
+
+//Sayfa yuklendiginde yeni soru soran fonksiyon
+window.onload=function(){
+    newQuestion();
+}
+
+answer.onclick=function(){
+    var ans,n1,n2;
+    n1=Number(Number1.textContent);
+    n2=Number(Number2.textContent);
+    switch(Operator.textContent){
+        case '+':ans=n1+n2;break;
+        case '-':ans=n1-n2;break;
+        case '*':ans=n1*n2;break;
+        case '/':ans=n1/n2;break;
+        default:break;
+    }
+
+    if(result.value==ans){
+        True.textContent=Number(True.textContent)+1;
+
+    }else{
+        False.textContent=Number(False.textContent)+1;
+    }
+
+    newQuestion();
+}
